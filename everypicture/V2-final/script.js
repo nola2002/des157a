6644396ecc721417a035e132ecbf8063dff4a2a0
nola2002 <nzimdars@ucdavis.edu>
2025-02-25 22:45:03 -0800

    everypic update

--- everypicture/V2-final/script.js
+++ everypicture/V2-final/script.js
@@ -2,19 +2,49 @@
     'use strict';
     console.log('reading js');
 
-    document.querySelector('.open').addEventListener('click', function(event){ 
+
+    // open overlay one
+    document.querySelector('#open1').addEventListener('click', function(event){ 
         event.preventDefault();
-        document.querySelector ('#overlay').className = 'showing';
-        document.querySelector ('#overlay').className = 'showing';
+        document.querySelector ('#overlay1').className = 'showing';
+    });
+    // close overlay 1
+    document.querySelector('.close').addEventListener('click', function(event){  
+        event.preventDefault();
+        document.querySelector ('#overlay1').className = 'hidden';
+    });
+    // 2
+    document.querySelector('#open2').addEventListener('click', function(event){ 
+        event.preventDefault();
+        document.querySelector ('#overlay2').className = 'showing';
     });
     document.querySelector('.close').addEventListener('click', function(event){  
         event.preventDefault();
-        document.querySelector ('#overlay').className = 'hidden';
+        document.querySelector ('#overlay2').className = 'hidden';
     });
-})();
+    // 3
+    document.querySelector('#open3').addEventListener('click', function(event){ 
+        event.preventDefault();
+        document.querySelector ('#overlay3').className = 'showing';
+    });
+    document.querySelector('.close').addEventListener('click', function(event){  
+        event.preventDefault();
+        document.querySelector ('#overlay3').className = 'hidden';
+    });
+
+
 
 document.addEventListener('keydown', function (event){
     if(event.key === 'Escape'){
-        document.querySelector('#overlay').className = 'hidden';
+        document.querySelector('#overlay1').className = 'hidden';
     }
-});
+}); 
+document.addEventListener('keydown', function (event){
+    if(event.key === 'Escape'){
+        document.querySelector('#overlay2').className = 'hidden';
+    }
+});   
+
+
+})();
+
