bdbfd6b08e6d657987fca07ae50900d27b8e34b3
nola2002 <nzimdars@ucdavis.edu>
2025-03-10 15:22:18 -0700

    game final

---
+++ game/game-final/script.js
@@ -0,0 +1,22 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const shape = document.querySelector('#shape1')
+    const options = document.querySelector('#')
+    const currentScore = document.querySelector('#score span');
+
+    //shape has a color
+
+    //user has 5 simialr color options
+
+    //on click the color the chose is selected, on submit the data is checked, correct answer = +1 point, wrong answer = -1 point
+
+    //new game button refreshes page
+
+    
+
+
+
+    
+})();
