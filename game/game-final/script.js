6e63fa42411ba7a0340fb0679256d049910ebfe1
nola2002 <nzimdars@ucdavis.edu>
2025-03-11 00:55:48 -0700

    game final

--- game/game-final/script.js
+++ game/game-final/script.js
@@ -3,18 +3,45 @@
     console.log('reading js');
 
     const shape = document.querySelector('#shape1')
-    const options = document.querySelector('#')
-    const currentScore = document.querySelector('#score span');
+    const circles = document.querySelector('circle')
+    const score = document.querySelector('#score span');
+    const submit = document.querySelector('submit')
+    const newGame = document.querySelector('button')
 
-    //shape has a color
+    let selectedColor =
+    let shownColor = 
+    let score = 
 
-    //user has 5 simialr color options
+    //generate the random RBG color for the shape
+    function createShownColor () {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+    }
+
+    //generate the random RBG color options for the circles
+
+
+
+    //shape is asigned a color RBG (3 numbers 0-255)
+    
+
+    //user is given 5 simialr color options to choose from (colors with RBG values numerically slighly off from the shape colors RBG values)
+    
 
     //on click the color the chose is selected, on submit the data is checked, correct answer = +1 point, wrong answer = -1 point
-
-    //new game button refreshes page
-
     
+
+    //refresh game 
+    function resetGame() {
+       
+    }
+    //new game button refreshes page
+    newGame.addEventListener('click', (){
+        score = 0;
+        resetGame();
+    });
+
 
 
 
